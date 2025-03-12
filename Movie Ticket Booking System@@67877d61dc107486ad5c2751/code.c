12e75646cfe52f6c7cc292229f00ccd39db262e7
sujal-lab <sujalkandari11@gmail.com>
2025-03-12 15:11:42 +0530

    Solution to Movie Ticket Booking System

---
+++ Movie Ticket Booking System@@67877d61dc107486ad5c2751/code.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        float a,b,c;
+        char d[20];
+        scanf("%c %c %f",&d,&e,&)
+        if(a>b&&b>c){
+            printf("Standard: %f, Premium: %f, VIP: %f",)
+        }
+        printf(" ")
+    }
+}
