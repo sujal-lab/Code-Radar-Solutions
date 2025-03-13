7b524d84fdc0f7783721526b4f1b79d1d137bfc3
sujal-lab <sujalkandari11@gmail.com>
2025-03-13 10:37:24 +0530

    Solution to Calculate Total and Average Marks of Students

---
+++ Calculate Total and Average Marks of Students@@67877d61dc107486ad5c2746/code.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main{}{
+    int a;
+    scanf("%d",&a);
+    int total_marks=0;
+    for(i=1;i<=a;i++){
+        int b;
+        char c[20];
+        float d;
+        scanf("%d %s %f",&b, c, &d);
+        total_marks+=d;
+    }
+    printf("Total Marks: %f",total_marks);
+    printf("Average Marks: %f",total_marks/a);
+}   
